69164b1160e4de4cc05da14e354ddd1ac1f71a4b
Serhii Husenko <sgusenko@gmail.com>
2025-07-23 22:49:14 +0300

    added beforeEach

--- Husenko_Serhii/tests/HW_6/coding_task2.spec.js
+++ Husenko_Serhii/tests/HW_6/coding_task2.spec.js
@@ -1,7 +1,10 @@
 import { test, expect } from "@playwright/test";
 
-test("ToDo item is created", async ({ page }) => {
+test.beforeEach(async ({page}) => {
     await page.goto("https://demo.playwright.dev/todomvc"); //does not work as baseUrl
+})
+
+test("ToDo item is created", async ({ page }) => {
     const input = page.getByRole("textbox", { text: "What needs to be done?" });
     await input.fill("Buy milk");
     await input.press("Enter");
