b430593f939e0247d0fdb6c5d10e6cd8b2d93b7e
Serhii Husenko <sgusenko@gmail.com>
2025-08-10 23:31:08 +0300

    changed logic to be sure of completed task based on name

--- Husenko_Serhii/tests/HW_7/coding_task3.spec.ts
+++ Husenko_Serhii/tests/HW_7/coding_task3.spec.ts
@@ -11,6 +11,19 @@ test("Testing Completion Logic", async ({ page }) => {
     await toDoInput.fill("UI-test 2");
     await toDoInput.press("Enter");
 
-    const firstTask = page.getByRole("checkbox", { name: "UI-test 1" });
-    await firstTask.check();
+    /*
+    const firstTask = page
+        .getByRole("checkbox", { name: "Toggle Todo" })
+        .first();
+    */
+    const parentListItem = page.getByTestId("todo-item").filter({
+        has: page.getByTestId("todo-title").filter({ hasText: "UI-test 1" }),
+    });
+    const firstTaskCheckbox = parentListItem.getByRole("checkbox", {
+        name: "Toggle Todo",
+    });
+    await firstTaskCheckbox.check();
+
+    await expect(firstTaskCheckbox).toBeChecked();
+    await expect(parentListItem).toHaveClass("completed");
 });
