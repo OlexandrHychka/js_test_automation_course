971898de808336a671cefd22e84d157d8e9e6b7b
Anna <anna.kashchuk@masterofcode.com>
2025-09-09 22:46:58 +0300

    HW_3: Coding Task #1: Basic Arithmetic Functions
    HW_3: Coding Task #1: Basic Arithmetic Functions

---
+++ Kashchuk_Anna/HW_3/CT_1.js
@@ -0,0 +1,19 @@
+// Напишіть функцію, яка повертає суму A та B.
+
+let a = 10
+let b = 20
+function sumAB(sumAB){
+    return sumAB
+}
+var myResult = sumAB(a + b)
+console.log("Сума A та B = " + myResult)
+
+// Напишіть функцію, яка повертає добуток (множення) A та B.
+
+let a1 = 5
+let b1 = 7
+function multiAB(multiAB){
+    return multiAB
+}
+var myResult2 = multiAB(a1 * b1)
+console.log("Множення A та B = " + myResult2)
