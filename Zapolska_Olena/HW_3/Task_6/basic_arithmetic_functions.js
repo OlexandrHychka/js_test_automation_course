6004abd34221064a9dbe55b23bb15f6befb08747
OlenaZapolska <olena.zapolska@masterofcode.com>
2025-11-11 15:20:51 +0200

    HW_3: Task_6

---
+++ Zapolska_Olena/HW_3/Task_6/basic_arithmetic_functions.js
@@ -0,0 +1,15 @@
+const value = {a: 13, b: 44}
+
+function Sum(value){
+    return value.a + value.b;
+}
+
+function Product(value){
+    return value.a * value.b;
+}
+
+console.log ('The sum of two numbers is ' + Sum(value))
+console.log ('The product of these numbers is ' + Product(value))
+
+
+
