f64f8d34b88b08de655dfac4ecf29e334bd40581
Hanna-irihina <hanna.irihina@masterofcode.com>
2025-11-18 22:15:42 +0200

    Правка для Task 24

--- Irihina_Hanna/HW_7/Task_24/tests/waiters.spec.ts
+++ Irihina_Hanna/HW_7/Task_24/tests/waiters.spec.ts
@@ -2,9 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test('waiters', async ({page}) => {
   await page.goto('https://demo.playwright.dev/todomvc');
+  await page.waitForSelector('.new-todo'); // 1 варіант очікування
+  
   const todoField = page.getByRole('textbox', { name: 'What needs to be done?' })
-  await todoField.waitFor();
-  expect(todoField).toBeVisible();
+  expect(todoField).toBeVisible();  // перевірка на видимість поля
 
   await todoField.fill('Очікування спрацьовує');
   await todoField.press('Enter');
