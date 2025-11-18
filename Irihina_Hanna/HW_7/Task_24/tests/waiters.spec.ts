537c845f6f419352e127546b6a3b43819088ea61
Hanna-irihina <hanna.irihina@masterofcode.com>
2025-11-18 23:10:27 +0200

    Task_25

--- Irihina_Hanna/HW_7/Task_24/tests/waiters.spec.ts
+++ Irihina_Hanna/HW_7/Task_24/tests/waiters.spec.ts
@@ -5,7 +5,7 @@ test('waiters', async ({page}) => {
   await page.waitForSelector('.new-todo'); // 1 варіант очікування
   
   const todoField = page.getByRole('textbox', { name: 'What needs to be done?' })
-  expect(todoField).toBeVisible();  // перевірка на видимість поля
+  expect(todoField).toBeVisible();  // 2 варіант очікування + перевірка на видимість поля
 
   await todoField.fill('Очікування спрацьовує');
   await todoField.press('Enter');
---
+++ Irihina_Hanna/HW_7/Task_25/tests/example.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('Completion logic', async ({page}) => {
+  await page.goto('https://demo.playwright.dev/todomvc');
+  const todoField = page.getByRole('textbox', { name: 'What needs to be done?' })
+  expect(todoField).toBeVisible();
+  const tasks = ["UI-тест", "UI-тест 2"]
+
+for (const task of tasks) {
+  await todoField.fill(task);
+  await todoField.press('Enter');
+   }
+
+ await page.getByLabel('Toggle Todo').first().check()
+ await  expect (page.getByRole('listitem').first()).toHaveClass('completed')
+  })
